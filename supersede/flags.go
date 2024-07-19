a4feaac3900367ad13f103667e0d75e453106620
sfomuseumbot <sfomuseumbot@localhost>
2024-07-19 16:02:46 -0700

    snapshot: chip away at logic for supersede command

--- supersede/flags.go
+++ supersede/flags.go
@@ -8,6 +8,10 @@ import (
 	"github.com/sfomuseum/go-flags/flagset"
 )
 
+var superseding_reader_uri string
+var superseding_writer_uri string
+var parent_reader_uri string
+
 var superseding_id int64
 var parent_id int64
 
@@ -16,8 +20,12 @@ var is_deprecated bool
 func DefaultFlagSet() *flag.FlagSet {
 
 	fs := flagset.NewFlagSet("properties")
-	fs.Int64Var(&parent_id, "parent_id", -1, "The parent ID to assign to the new record.")
-	fs.Int64Var(&superseding_id, "superseding_id", -1, "The ID to supersede each record with. If `-1` then each record will be cloned and the new ID of the clone will be used as the superseding_id.")
+	fs.Int64Var(&parent_id, "parent-id", -1, "The parent ID to assign to the new record.")
+	fs.Int64Var(&superseding_id, "superseding-id", -1, "The ID to supersede each record with. If `-1` then each record will be cloned and the new ID of the clone will be used as the superseding_id.")
+
+	fs.StringVar(&superseding_reader_uri, "superseding-reader-uri", "null://", "A valid whosonfirst/go-reader URI used to load records that are doing the superseding. Required if -superseding-id is not `-1`.")
+	fs.StringVar(&superseding_writer_uri, "superseding-writer-uri", "null://", "A valid whosonfirst/go-writer URI used to update records that are doing the superseding. Required if -superseding-id is not `-1`.")
+	fs.StringVar(&parent_reader_uri, "parent-reader-uri", "null://", "A valid whosonfirst/go-reader URI used to load parent records if -superseded-id is `-1`. Required if -parent-id is not `-1`.")
 
 	fs.BoolVar(&is_deprecated, "deprecated", false, "Each record being superseded should also be marked as deprecated.")
 
--- supersede/supersede.go
+++ supersede/supersede.go
@@ -7,13 +7,17 @@ import (
 	"slices"
 	"time"
 
+	"github.com/whosonfirst/go-reader"
 	"github.com/whosonfirst/go-whosonfirst-export/v2"
 	"github.com/whosonfirst/go-whosonfirst-feature/properties"
 	wof_id "github.com/whosonfirst/go-whosonfirst-id"
+	wof_reader "github.com/whosonfirst/go-whosonfirst-reader"
+	wof_writer "github.com/whosonfirst/go-whosonfirst-writer/v3"
+	"github.com/whosonfirst/go-writer/v3"
 	"github.com/whosonfirst/wof"
-	"github.com/whosonfirst/wof/reader"
+	cli_reader "github.com/whosonfirst/wof/reader"
 	"github.com/whosonfirst/wof/uris"
-	"github.com/whosonfirst/wof/writer"
+	cli_writer "github.com/whosonfirst/wof/writer"
 )
 
 type SupersedeCommand struct {
@@ -39,6 +43,9 @@ func (c *SupersedeCommand) Run(ctx context.Context, args []string) error {
 	fs_uris := fs.Args()
 
 	var id_provider wof_id.Provider
+	var superseding_reader reader.Reader
+	var superseding_writer writer.Writer
+	var parent_reader reader.Reader
 
 	if superseding_id == -1 {
 
@@ -49,22 +56,49 @@ func (c *SupersedeCommand) Run(ctx context.Context, args []string) error {
 		}
 
 		id_provider = pr
+
+		r, err := reader.NewReader(ctx, superseding_reader_uri)
+
+		if err != nil {
+			return fmt.Errorf("Failed to create superseding reader, %w", err)
+		}
+
+		superseding_reader = r
+
+		wr, err := writer.NewWriter(ctx, superseding_writer_uri)
+
+		if err != nil {
+			return fmt.Errorf("Failed to create superseding writer, %w", err)
+		}
+
+		superseding_writer = wr
+	}
+
+	if parent_id != -1 {
+
+		r, err := reader.NewReader(ctx, parent_reader_uri)
+
+		if err != nil {
+			return fmt.Errorf("Failed to create parent reader, %w", err)
+		}
+
+		parent_reader = r
 	}
 
 	cb := func(ctx context.Context, cb_uri string) error {
 
-		body, err := reader.BytesFromURI(ctx, cb_uri)
+		body, err := cli_reader.BytesFromURI(ctx, cb_uri)
 
 		if err != nil {
 			return fmt.Errorf("Failed to open %s for reading, %w", cb_uri, err)
 		}
 
 		old_id, err := properties.Id(body)
-		
+
 		if err != nil {
 			return fmt.Errorf("Failed to derive ID for %s, %w", cb_uri, err)
 		}
-		
+
 		cb_superseding_id := superseding_id
 
 		now := time.Now()
@@ -80,14 +114,34 @@ func (c *SupersedeCommand) Run(ctx context.Context, args []string) error {
 			}
 
 			updates := map[string]interface{}{
-				"id": new_id,
-				"properties.wof:id": new_id,
+				"id":                           new_id,
+				"properties.wof:id":            new_id,
 				"properties.edtf:inception":    now.Format("2006-01-02"),
 				"properties.edtf:cessation":    "..",
 				"properties.mz:is_current":     1,
 				"properties.wof:supersedes":    []int64{old_id},
 				"properties.wof:superseded_by": []int64{},
 			}
+
+			// Read properties from parent?
+
+			if parent_id != -1 {
+
+				parent_body, err := wof_reader.LoadBytes(ctx, parent_reader, parent_id)
+
+				if err != nil {
+					return fmt.Errorf("Failed to load record for parent ID, %w", err)
+				}
+
+				parent_hierarchies := properties.Hierarchies(parent_body)
+				parent_country := properties.Country(parent_body)
+
+				updates["properties.wof:parent_id"] = parent_id
+				updates["properties.wof:hierarchy"] = parent_hierarchies
+				updates["properties.wof:country"] = parent_country
+			}
+
+			// Else, re-PIP?
 
 			_, new_body, err := export.AssignPropertiesIfChanged(ctx, body, updates)
 
@@ -98,10 +152,46 @@ func (c *SupersedeCommand) Run(ctx context.Context, args []string) error {
 			// write stuff TBD...
 			slog.Debug(string(new_body))
 
+			_, err = wof_writer.WriteBytes(ctx, superseding_writer, new_body)
+
+			if err != nil {
+				return fmt.Errorf("Failed to write new record (%d) derived from %s, %w", new_id, cb_uri, err)
+			}
+
 			cb_superseding_id = new_id
+
 		} else {
-			
-			// Load other ID and assign old_id to wof:supersedes
+
+			superseding_body, err := wof_reader.LoadBytes(ctx, superseding_reader, cb_superseding_id)
+
+			if err != nil {
+				return fmt.Errorf("Failed to load record for superseding ID, %w", err)
+			}
+
+			supersedes := properties.Supersedes(superseding_body)
+
+			if !slices.Contains(supersedes, old_id) {
+				supersedes = append(supersedes, old_id)
+			}
+
+			updates := map[string]interface{}{
+				"properties.wof:supersede": supersedes,
+			}
+
+			has_changes, new_body, err := export.AssignPropertiesIfChanged(ctx, superseding_body, updates)
+
+			if err != nil {
+				return fmt.Errorf("Failed to assign new properties to superseding ID for %s, %w", cb_uri, err)
+			}
+
+			if has_changes {
+
+				_, err := wof_writer.WriteBytes(ctx, superseding_writer, new_body)
+
+				if err != nil {
+					return fmt.Errorf("Failed to write changes to superseding ID for %s, %w", cb_uri, err)
+				}
+			}
 		}
 
 		superseded_by := properties.SupersededBy(body)
@@ -128,7 +218,7 @@ func (c *SupersedeCommand) Run(ctx context.Context, args []string) error {
 
 		if has_changes {
 
-			err := writer.Write(ctx, cb_uri, new_body)
+			err := cli_writer.Write(ctx, cb_uri, new_body)
 
 			if err != nil {
 				return fmt.Errorf("Failed to write new properties to %s, %w", cb_uri, err)
