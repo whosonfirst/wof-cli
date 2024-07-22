3fea42faaffa1813c51bed2316d0b6e2ec38283a
sfomuseumbot <sfomuseumbot@localhost>
2024-07-22 11:53:51 -0700

    docs for supersede command

--- supersede/flags.go
+++ supersede/flags.go
@@ -19,7 +19,7 @@ var is_deprecated bool
 
 func DefaultFlagSet() *flag.FlagSet {
 
-	fs := flagset.NewFlagSet("properties")
+	fs := flagset.NewFlagSet("supersede")
 	fs.Int64Var(&parent_id, "parent-id", -1, "The parent ID to assign to the new record.")
 	fs.Int64Var(&superseding_id, "superseding-id", -1, "The ID to supersede each record with. If `-1` then each record will be cloned and the new ID of the clone will be used as the superseding_id.")
 
@@ -30,8 +30,8 @@ func DefaultFlagSet() *flag.FlagSet {
 	fs.BoolVar(&is_deprecated, "deprecated", false, "Each record being superseded should also be marked as deprecated.")
 
 	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Print one or more properties for one or more Who's On First IDs.\n")
-		fmt.Fprintf(os.Stderr, "Usage:\n\t %s path(N) path(N)\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Supersede one or more Who's On First record and update the records superseding them.\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n\t %s [options] path(N) path(N)\n", os.Args[0])
 		fs.PrintDefaults()
 	}
 
